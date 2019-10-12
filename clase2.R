b3004cc1420578f30cc34c1911850be2d709fe41
Tavo <gariosp@unal.edu.co>
2019-10-12 19:36:47 -0300

    Adding tidyr and lubridate functions

--- clase2.R
+++ clase2.R
@@ -82,6 +82,76 @@ monthlyMeanIndex <- Datos %>%
 
 
 
+#*********Dplyr joins************#
+#Left Join
+Ponderadores <- data.frame(GRUPO = c("Privado_Registrado","Público","Privado_No_Registrado"),
+                           PONDERADOR = c(50.16,29.91,19.93))
+
+Datos_join <- Datos %>% 
+  left_join(.,Ponderadores, by = "GRUPO")
+Datos_join
+
+#Calculando el índice ponderado con weighted.mean 
+Datos_Indice_Gral <- Datos_join %>% 
+  group_by(FECHA) %>% 
+  summarise(Indice_Gral = weighted.mean(INDICE,w = PONDERADOR))
+Datos_Indice_Gral
+
+
+#Tidyr facilitará el emprolijamiento de los datos
+#aather pasar los datos de forma horizontal a una forma vertical.
+#spread pasar los datos de forma vertical a una forma horizontal.
+
+
+#Utilzamos un conjunto de datos que viene con la librería datasets
+library(datasets)
+
+data(iris)
+iris <- iris %>% 
+  mutate(id = 1:nrow(.)) %>%  #le agrego un ID
+  select(id, everything()) # lo acomodo para que el id este primero. 
+
+iris
+
+#Usando gather
+iris_vertical <- iris %>% gather(., # el . llama a lo que esta atras del %>% 
+                                 key   = Variables,
+                                 value = Valores,
+                                 2:5) #le indico que columnas juntar
+iris_vertical
+
+#usando spread
+iris_horizontal <- iris_vertical %>%
+  spread(. ,
+         key   = Variables, #la llave es la variable que va a dar los nombres de columna
+         value = Valores) #los valores con que se llenan las celdas
+iris_horizontal
+
+#Lubridate para trabajar con fechas
+#Para cambio de formato
+fecha  <- "04/12/92 17:35:16"
+fecha
+
+fecha  <- dmy_hms(fecha)
+fecha
+
+#parse_date_time permite operaciones un poco más complejas
+fecha2  <- "Dec-92"
+fecha2 <- parse_date_time(fecha2, orders = 'my')
+fecha2
+
+#extraer información de la fecha
+year(fecha) # Obtener el año
+month(fecha) #Obtener el mes
+day(fecha) # Obtener el día
+wday(fecha, label = TRUE) #Obtener el nombre del día
+hour(fecha) #Obtener la hora
+
+#operaciones
+# Sumo dos días 
+fecha + days(2)
+# Resto 1 semana y dos horas
+fecha - (weeks(1) + hours(2))
 
 
 
