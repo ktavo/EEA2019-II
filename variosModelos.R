2e2feb0b940256ebd025cb61dd148183f3ddf205
Tavo <gariosp@unal.edu.co>
2019-11-02 13:07:16 -0300

    Adding clase 2 de múltiples modelos

--- variosModelos.R
+++ variosModelos.R
@@ -28,6 +28,33 @@ gapminder %>%
 
 
 #Para calcular el modelo lineal para algunos países
+#Nueva Zelanda
+#Partimos de:
+nz <- filter(gapminder, country == "New Zealand")
+nz %>% 
+  ggplot(aes(year, lifeExp)) + 
+  geom_line() + 
+  ggtitle("Y = ")
+
+#Generamos el modelo lineal
+nz_mod <- lm(lifeExp ~ year, data = nz)
+nz %>% 
+  add_predictions(nz_mod) %>%
+  ggplot(aes(year, pred)) + 
+  geom_line() + 
+  ggtitle(expression(beta[0] + beta[1]*x))
+
+
+nz %>% 
+  add_residuals(nz_mod) %>% 
+  ggplot(aes(year, resid)) + 
+  geom_hline(yintercept = 0, colour = "white", size = 3) + 
+  geom_line() + 
+  ggtitle(expression(+ epsilon))
+
+
+
+#Colombia
 #Partimos de:
 expectativaColombia <- filter(gapminder, country == "Colombia")
 expectativaColombia %>% 
@@ -112,6 +139,71 @@ resids %>%
 
 #Clase 2-Nov
 #Muchos modelos clase 2
+
+glance(modeloColombia)
+glance(nz_mod)
+
+#Ahira con mutate y unnest creamos un modelo por país
+by_country %>% 
+  mutate(glnc = map(model, glance)) %>% 
+  unnest(glnc)
+
+#Si queremos sacarnos de encima las columnas de listas (data,model,resids),
+#necesitamos agregar .drop = TRUE: DEPRECATED –> ahora es con select
+glnc <- by_country %>% 
+  mutate(glnc = map(model, glance)) %>% 
+  unnest(glnc) %>% 
+  select(-c(data,model,resids))
+glnc
+
+#Organizamos por las variables que tenemos
+
+#r.suqared me muestra para quienes está funcionando mejor el modelo
+#Para ver para que paises funciona peor (para áfrica pareciera funcionar peor)
+glnc %>% 
+  arrange(r.squared)
+#Para ver para que paises funciona mejor (para las américas y europa pareciera funcionar mejor)
+glnc %>% 
+  arrange(desc(r.squared))
+#O por statistic que parece interesante
+glnc %>% 
+  arrange(desc(statistic))
+
+#Graficamente comparando por continente, vemos que en África tenemos mucha variabilidad del modelo
+#La variabilidad para el de oceanía pareciera mostrar que anda bien (o que tiene pocos datos)
+glnc %>% 
+  ggplot(aes(continent, r.squared, fill = continent)) + 
+  geom_boxplot()+
+  theme(legend.position = "none")
+
+#En una gráfica diferente
+glnc %>% 
+  ggplot(aes(continent, r.squared)) + 
+  geom_jitter(width = 0.5)
+
+#Si queremos ver los 6 peores modelos
+#Aquí vemos dos efectos principales: las tragedias de la epidemia de VIH / SIDA 
+#y el genocidio de Rwanda.
+
+bad_fit <- filter(glnc, r.squared < 0.25)
+
+gapminder %>% 
+  semi_join(bad_fit, by = "country") %>% 
+  ggplot(aes(year, lifeExp, colour = country)) +
+  geom_line()
+
+
+#El comando tidy(model) nos permite obtener la salida del modelo lineal de forma prolija.
+#En el caso particular de un modelo. El output tradicional se vería de esta forma
+
+nz_mod <- lm(lifeExp ~ year, data = nz)
+summary(nz_mod)
+
+modeloColombia <- lm(lifeExp ~ year, data = expectativaColombia)
+summary(modeloColombia)
+
+
+
 
 #El que tiene el coeficiente mpás grande es Omán, que tiene un cambio mayor en 
 #la expectativa de vida
