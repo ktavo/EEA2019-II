17b755342387258eb58f9ed7c8d18edbe4519c02
Tavo <gariosp@unal.edu.co>
2019-11-16 19:27:14 -0300

    Adding linear basic functions to measure distance

--- modeloLinealBasico.R
+++ modeloLinealBasico.R
@@ -30,15 +30,58 @@ ggplot(sim1, aes(x, y)) +
   geom_abline(aes(intercept = a1, slope = a2), data = models, alpha = 1/4) +
   geom_point() 
 
+#Se buscaría disminuir las distancias de los puntos a la racta que generemos
+
+dist1 <- sim1 %>% 
+  mutate(
+    dodge = rep(c(-1, 0, 1) / 20, 10),
+    x1 = x + dodge,
+    pred = 7 + x1 * 1.5
+  )
+
+ggplot(dist1, aes(x1, y)) + 
+  geom_abline(intercept = 7, slope = 1.5, colour = "grey40") +
+  geom_point(colour = "grey40") +
+  geom_linerange(aes(ymin = y, ymax = pred), colour = "#3366FF") 
 
 
+#Generamos una función para esto
+
+linealmodel <- function(coefficients, data) 
+{
+  dist1 <- data %>% 
+    mutate(
+      dodge = rep(c(-1, 0, 1) / 20, 10),
+      x1 = x + dodge,
+      pred = coefficients[1] + x1 * coefficients[2]
+    )
+  
+  ggplot(dist1, aes(x1, y)) + 
+    geom_abline(intercept = coefficients[1], slope = coefficients[2], colour = "grey40") +
+    geom_point(colour = "grey40") +
+    geom_linerange(aes(ymin = y, ymax = pred), colour = "#FF6600") 
+  
+ 
+   
+}
+linealmodel(c(7, 1.5), sim1)
 
 
-
-
-
-
-
+#Ahora haremos una función para calcular las distancias
+measure_distance <- function(mod, data) 
+{
+  mod
+  data$x
+  data <- data %>% mutate(lineValue = mod[1] + (mod[2]*data$x))
+  data <- data %>% mutate(diff = (lineValue - data$y)^2)
+  sumaDiffs <- data %>% 
+    summarise(sumaDiffs = sum(diff))
+  sumaDiffs <- sumaDiffs/length(data$x)
+  sumaDiffs <- sqrt(sumaDiffs)
+}
+distance <- measure_distance(c(7, 1.5), sim1)
+distance
+#extendedData <- measure_distance(c(7, 1.5), sim1)
 
 
 
